f4c20c2504ee04500df4fb9622ebc048f02336ea
jiwonsudo <wondev112@gmail.com>
2025-12-26 20:00:24 +0900

    Design: Add fonts.ts to apply font-styles & upload font files

---
+++ app/fonts.ts
@@ -0,0 +1,21 @@
+import localFont from 'next/font/local';
+
+const bookkBold = localFont({
+  src: '../public/fonts/BookkMyungjo_Bold.woff',
+  display: 'swap',
+  variable: '--font-bookk-bold',
+})
+
+const bookkLight = localFont({
+  src: '../public/fonts/BookkMyungjo_Light.woff',
+  display: 'swap',
+  variable: '--font-bookk-light',
+})
+
+const poppinsSemiBold = localFont({
+  src: '../public/fonts/Poppins-SemiBold.woff',
+  display: 'swap',
+  variable: '--font-bookk-poppin',
+})
+
+export const fonts = { bookkBold, bookkLight, poppinsSemiBold };
