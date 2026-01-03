647fa54bdca403ddd3f15f79a2453f6466a82eaa
jiwonsudo <wondev112@gmail.com>
2026-01-04 02:35:47 +0900

    Feat: complete landingpage design

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <section className="flex justify-center items-center text-neutral-700 text-center h-24 bg-neutral-300/30 border border-neutral-300/50 shadow-[0_0_10px_2px_#D4D4D480]">
+      <div>
+        <p className="text-lg">Mychelin&#8482;</p>
+        <p className="mt-1 text-sm">Copyright 2026. Jiwon Jeong All rights reserved.</p>
+      </div>
+    </section>
+  );
+}
