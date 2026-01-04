063e554ef39a0d80783e0e05fcf86695baad99d7
jiwonsudo <wondev112@gmail.com>
2026-01-04 16:30:10 +0900

    Refactor: delete unused div

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { CallToAction } from "./components/domain/landing-page/CallToAction";
 export default function Home() {
   return (
     <div>
-      <div className="w-full"></div>
       <Hero></Hero>
       <Features></Features>
       <CallToAction></CallToAction>
